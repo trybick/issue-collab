d326559e24baf7e39c7edf26a163718874cb4ac4
trybick <t.rybicki@gmail.com>
2018-10-31 14:57:00 -0400

    Try if statement and remove it around items declaration to fix map undefined error

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,23 +2,23 @@ import React from 'react'
 
 const SearchResults = (props) => {
 
-    const items = props.results.items
+    const items = props.results.items.map(item => {
+        return (
+            <div>{item.url}</div>
+        )
+    })
 
-    
+
 
     return (
 
-        
-
-
+    
         <div className="results">
           <h3>Results:</h3>
 
-          
-          
           {console.log('props', items)}
           
-          
+        {items}
 
 
 
