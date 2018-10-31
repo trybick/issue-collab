b6cacb0fab80daa9853e46fdc87367c9e4f2a104
trybick <t.rybicki@gmail.com>
2018-10-31 12:59:17 -0400

    Complete basic search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,30 @@
 import React from 'react'
 
-const SearchBar = () => {
+class SearchBar extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        bill: {
+          name: '',
+          amount: '',
+          due: '1' // avoids issue when onChange doesn't register
+        }
+      }
+    }
 
-    return (
+
+    render() {
+        const {bill} = this.state
+    
+        return (
         <div>
             <h3 className="section-title">Enter Your Search</h3>
             <input type="text" name="search-input"></input>
         </div>
     )
+    }
 }
+
+
 
 export default SearchBar
