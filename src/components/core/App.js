7fc5b0368bef34ae270a7e13e1ab3f924bb19fdc
trybick <t.rybicki@gmail.com>
2019-02-23 19:27:24 -0500

    Create new folder structure and remove serviceWorker

---
+++ src/components/core/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+import Title from './Title';
+
+import SearchBar from '../search/SearchBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='app-wrapper'>
+        <Title />
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
