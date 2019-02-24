532fb9a8f897be62f6ec050051ae3ad909cc59e2
trybick <t.rybicki@gmail.com>
2019-02-24 14:57:02 -0500

    Change most divs to paragraph tags in SearchResults

--- src/components/search/SearchResults.js
+++ src/components/search/SearchResults.js
@@ -5,7 +5,7 @@ const SearchResults = props => {
   const { results } = props;
 
   const items =
-    results.items &&
+    results.items[0] &&
     results.items.map(item => {
       const htmlUrl = item.html_url.split('/');
       const userName = htmlUrl[3];
@@ -13,27 +13,22 @@ const SearchResults = props => {
 
       return (
         <div className='result' key={item.id}>
-          <div>
-            {/* Title with hyperlink */}
-            <a href={item.html_url} target='_blank' rel='noopener noreferrer'>
-              {item.title}
-            </a>
-          </div>
+          {/* Hyperlinked title */}
+          <a href={item.html_url} target='_blank' rel='noopener noreferrer'>
+            {item.title}
+          </a>
 
+          {/* First 300 characters of body */}
           <p>
-            {/* First 300 characters of body */}
             {item.body.length === 0 ? '(no text)' : item.body.substr(0, 300)}
             {item.body.length > 300 ? '...' : null}
           </p>
 
-          {/* User and Repo name */}
           <div>{userName + '/' + repoName}</div>
-
-          {/* How long ago created */}
           <div>{moment(item.created_at).fromNow()}</div>
 
           {/* Some issues have an array of labels */}
-          <div>
+          <p>
             {item.labels.map(label => {
               const labelColor = '#' + label.color;
               return (
@@ -42,7 +37,7 @@ const SearchResults = props => {
                 </span>
               );
             })}
-          </div>
+          </p>
 
           <br />
           <br />
