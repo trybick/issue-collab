fe07326df481ca1c9073f2a7132244060e486aa1
trybick <t.rybicki@gmail.com>
2019-03-10 15:53:26 -0400

    Add url for testing to app screen

--- src/components/core/App.js
+++ src/components/core/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import Title from './Title';
-import SearchResults from '../search/SearchResults';
-import '../../style.scss';
-import GroupedToggles from '../search/GroupedToggles.js';
+import React from "react";
+import Title from "./Title";
+import SearchResults from "../search/SearchResults";
+import "../../style.scss";
+import GroupedToggles from "../search/GroupedToggles.js";
 
 class SearchBar extends React.Component {
   constructor(props) {
@@ -10,10 +10,10 @@ class SearchBar extends React.Component {
     this.state = {
       enabledLanguages: [],
       enabledLabels: [],
-      providedText: '',
-      issueState: 'open',
+      providedText: "",
+      issueState: "open",
       results: {},
-      totalResults: ''
+      totalResults: ""
     };
   }
 
@@ -25,41 +25,40 @@ class SearchBar extends React.Component {
     // const availableLabels = ['javascript', 'bug'];
     // Can this array be removed? The toggle buttons should have text we are expecting.
 
-      const stateArray = [...this.state.enabledLabels];
-      const index = stateArray.indexOf(event.target.value)
-      if (index !== -1) {
-        stateArray.splice(index, 1);
-        this.setState({enabledLabels: stateArray});
-      } else {
-        this.setState(
-        {
-          enabledLabels: [...this.state.enabledLabels, event.target.name]
-        })
-      }
+    const stateArray = [...this.state.enabledLabels];
+    const index = stateArray.indexOf(event.target.value);
+    if (index !== -1) {
+      stateArray.splice(index, 1);
+      this.setState({ enabledLabels: stateArray });
+    } else {
+      this.setState({
+        enabledLabels: [...this.state.enabledLabels, event.target.name]
+      });
+    }
     // TODO: create function shouldAdd - check if label has already been added and is in array
   };
 
   formatUrl = () => {
     const { issueState, enabledLabels, providedText } = this.state;
 
-    const baseUrl = 'https://api.github.com/search/issues?q=type:issue';
-    const sortOptions = '&sort=created&order=desc&per_page=30';
+    const baseUrl = "https://api.github.com/search/issues?q=type:issue";
+    const sortOptions = "&sort=created&order=desc&per_page=30";
 
     // IDEA: can this work for creating URL from object?
     // var queryString = Object.keys(params).map(key => key + '=' + params[key]).join('&');
 
-    let completeLabels = '+';
+    let completeLabels = "+";
     if (enabledLabels.length === 1) {
       completeLabels = `+label:${enabledLabels[0]}`;
     } else if (enabledLabels.length > 1) {
       completeLabels = enabledLabels.map = label => {
-        return '+label:$' + label;
+        return "+label:$" + label;
       };
     }
 
-    let textToSend = '';
-    if (providedText !== '') {
-      textToSend = providedText + '+';
+    let textToSend = "";
+    if (providedText !== "") {
+      textToSend = providedText + "+";
     }
 
     const completeUrl =
@@ -78,26 +77,25 @@ class SearchBar extends React.Component {
     const response = await fetch(urlToSend);
     const json = await response.json();
 
-    this.setState(
-      { results: json, },
-      () => console.log('results', this.state.results)
+    this.setState({ results: json, urlToSend: urlToSend }, () =>
+      console.log("results", this.state.results)
     );
 
-    console.log('url', urlToSend);
+    console.log("url", urlToSend);
   };
 
   render() {
     const { results, providedText } = this.state;
 
     return (
-      <div className='wrapper'>
+      <div className="wrapper">
         <Title />
 
-        <div className='searchbar'>
+        <div className="searchbar">
           <h3>Enter Your Search</h3>
           <input
-            type='text'
-            name='search-text'
+            type="text"
+            name="search-text"
             value={providedText}
             onChange={this.handleTextChange}
           />
@@ -106,8 +104,9 @@ class SearchBar extends React.Component {
 
         <GroupedToggles onToggle={this.onToggle} />
 
-        {results.items && 
-          <SearchResults results={results} />}
+        <h3>{this.state.urlToSend}</h3>
+
+        {results.items && <SearchResults results={results} />}
       </div>
     );
   }
