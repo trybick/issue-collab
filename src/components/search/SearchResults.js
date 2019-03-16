35268eef6282113f36fcd933add0c5e0f9543d6d
trybick <t.rybicki@gmail.com>
2019-03-16 16:44:39 -0400

    Change bug label color to remove variable

--- src/components/search/SearchResults.js
+++ src/components/search/SearchResults.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import moment from 'moment';
+import React from "react";
+import moment from "moment";
 
 const SearchResults = ({ results }) => {
   const searchResults =
     results.items[0] &&
     results.items.map(item => {
-      const htmlUrl = item.html_url.split('/');
+      const htmlUrl = item.html_url.split("/");
       const userName = htmlUrl[3];
       const repoName = htmlUrl[4];
       const issueAge = moment(item.created_at).fromNow();
@@ -13,34 +13,33 @@ const SearchResults = ({ results }) => {
       let bodyText;
       if (item.body) {
         if (item.body.length === 0) {
-          bodyText = '(no text provided)';
+          bodyText = "(no text provided)";
         } else if (item.body.length < 300) {
           bodyText = item.body;
         } else if (item.body.length > 300) {
-          bodyText = item.body.substr(0, 300) + '...';
+          bodyText = item.body.substr(0, 300) + "...";
         }
       }
 
       const mappedLabels = item.labels.map(label => {
-        const labelColor = '#' + label.color;
         return (
-          <span key={label.id} style={{ color: labelColor }}>
+          <span key={label.id} style={{ color: `#${label.color}` }}>
             {label.name}
           </span>
         );
       });
 
       return (
-        <div className='result' key={item.id}>
-          <img src={item.user.avatar_url} width='50px' alt='avatar' />
+        <div className="result" key={item.id}>
+          <img src={item.user.avatar_url} width="50px" alt="avatar" />
 
-          <a href={item.html_url} target='_blank' rel='noopener noreferrer'>
+          <a href={item.html_url} target="_blank" rel="noopener noreferrer">
             {item.title}
           </a>
 
           <p>{bodyText}</p>
 
-          <div>{userName + '/' + repoName}</div>
+          <div>{userName + "/" + repoName}</div>
           <div>{issueAge}</div>
 
           <p>{mappedLabels}</p>
@@ -52,11 +51,11 @@ const SearchResults = ({ results }) => {
     });
 
   return (
-    <div className='results'>
+    <div className="results">
       <h4>Total results: {results.total_count.toLocaleString()}</h4>
       {searchResults}
     </div>
-  )
+  );
 };
 
 export default SearchResults;
