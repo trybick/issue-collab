ca51e0823cc29fb715e5303262e39dd92e9cb4ae
trybick <t.rybicki@gmail.com>
2019-04-06 19:30:00 -0400

    Fix toggle button

--- src/components/core/Button.js
+++ src/components/core/Button.js
@@ -2,23 +2,14 @@ import React from 'react';
 import classNames from "classnames";
 
 class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isToggled: false
-    };
-  }
-
   render() {
     const { activeLabels, text, name, onClick } = this.props;
-    const { isToggled } = this.state;
+    let isToggled;
 
-    console.log('activeLabels:', activeLabels)
-
-
-    if (activeLabels === undefined) {
-    } else if (activeLabels.includes(name)) {
-        this.setState({ isToggled: true });
+    if (activeLabels !== undefined) {
+      if (activeLabels.includes(name)) {
+        isToggled = true;
+      }
     } 
 
     const btnClass = classNames({
