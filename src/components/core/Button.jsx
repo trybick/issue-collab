9505d4f504816c4732a45fb28e413ff35ad614e1
trybick <t.rybicki@gmail.com>
2019-05-05 11:56:28 -0400

    Add ternary to button component for wrapper

--- src/components/core/Button.jsx
+++ src/components/core/Button.jsx
@@ -4,9 +4,8 @@ import classNames from 'classnames';
 const Button = ({ classNameWrapper, typeName, children, className, onClick }) => {
   const btnClasses = classNames('btn', className);
   const wrapperClasses = classNames('btn-wrapper', classNameWrapper);
-  console.log('wrapperClasses:', wrapperClasses)
 
-  return (
+  const Component = classNameWrapper ? (
     <div className={wrapperClasses}>
       {/* type prop not regonized correctly https://github.com/yannickcr/eslint-plugin-react/issues/1555 */}
       {/* eslint-disable-next-line react/button-has-type */}
@@ -14,7 +13,14 @@ const Button = ({ classNameWrapper, typeName, children, className, onClick }) =>
         {children}
       </button>
     </div>
+  ) : (
+    // eslint-disable-next-line react/button-has-type
+    <button className={btnClasses} onClick={onClick} type={typeName}>
+      {children}
+    </button>
   );
+
+  return Component;
 };
 
 export default Button;
