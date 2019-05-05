54b9da313896ff648401d16a91c8b10f95aa59d8
trybick <t.rybicki@gmail.com>
2019-05-05 11:59:50 -0400

    Put ternary into normal return

--- src/components/core/Button.jsx
+++ src/components/core/Button.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/button-has-type */
+// type prop not recogonized correctly: https://github.com/yannickcr/eslint-plugin-react/issues/1555
 import React from 'react';
 import classNames from 'classnames';
 
@@ -5,22 +7,17 @@ const Button = ({ classNameWrapper, typeName, children, className, onClick }) =>
   const btnClasses = classNames('btn', className);
   const wrapperClasses = classNames('btn-wrapper', classNameWrapper);
 
-  const Component = classNameWrapper ? (
+  return classNameWrapper ? (
     <div className={wrapperClasses}>
-      {/* type prop not regonized correctly https://github.com/yannickcr/eslint-plugin-react/issues/1555 */}
-      {/* eslint-disable-next-line react/button-has-type */}
       <button className={btnClasses} onClick={onClick} type={typeName}>
         {children}
       </button>
     </div>
   ) : (
-    // eslint-disable-next-line react/button-has-type
     <button className={btnClasses} onClick={onClick} type={typeName}>
       {children}
     </button>
   );
-
-  return Component;
 };
 
 export default Button;
