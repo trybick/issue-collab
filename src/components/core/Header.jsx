594d061a8d101039236d7bf3feb76b19b58e2572
trybick <t.rybicki@gmail.com>
2019-05-09 07:00:59 -0400

    Update header title

--- src/components/core/Header.jsx
+++ src/components/core/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="header">
       {/* <img src="../../images/logo.png" alt="header-logo" /> */}
-      <h2 className="page-title">Issues Collab</h2>
+      <h2 className="page-title">Issue Collab</h2>
       {/* <h4>github issue searcher</h4> */}
     </div>
   );
