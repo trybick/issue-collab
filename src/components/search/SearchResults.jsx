1518de301eb54e98d22312818bafbdb32dae0d07
trybick <t.rybicki@gmail.com>
2019-05-12 20:21:27 -0400

    Create statuses directory

--- src/components/search/SearchResults.jsx
+++ src/components/search/SearchResults.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import moment from 'moment';
-import NoResultsMessage from './NoResultsMessage';
+import NoResultsMessage from '../statuses/NoResultsMessage';
 
 const SearchResults = ({ results }) => {
   const formattedResults =
