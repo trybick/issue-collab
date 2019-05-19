ccd3e396d9a086ba059981f4b284f66ffd208a5e
trybick <t.rybicki@gmail.com>
2019-05-19 18:26:18 -0400

    Add blue and black colors to title

--- src/components/core/Header.jsx
+++ src/components/core/Header.jsx
@@ -51,7 +51,10 @@ const Header = () => {
   return (
     <div className="header">
       {/* <img src="../../images/logo.png" alt="header-logo" /> */}
-      <h2 className="page-title">Issue Collab</h2>
+      <h2 className="page-title">
+        <span style={{ color: '#3498db' }}>Issue</span>-
+        <span style={{ color: 'black' }}>Collab</span>
+      </h2>
       {githubCorner}
     </div>
   );
