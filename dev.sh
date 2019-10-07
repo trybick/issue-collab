a7d8d4a97cb4156f36a0f4f523dc90c2b34143f3
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2019-10-07 22:20:31 +0200

    Add containerized environment for the application

---
+++ dev.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -xe
+
+IMAGE_NAME="issue-collab"
+
+docker build --tag ${IMAGE_NAME} --file Dockerfile .
+docker rm --force ${IMAGE_NAME}-cont || true
+docker run \
+    --interactive \
+    --tty \
+    --name ${IMAGE_NAME}-cont \
+    --publish 3000:3000 \
+    ${IMAGE_NAME}
