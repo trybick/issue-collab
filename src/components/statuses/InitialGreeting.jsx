0f0c45ba7bb5f62562e21039573e5798dcd6ae1a
Marhatta <vishal.marhatta@gmail.com>
2019-10-09 16:25:42 +0530

    Added a gradient to text,changed button colors,updated scale of the button to .8 when button is active which gives it a little effect

--- src/components/statuses/InitialGreeting.jsx
+++ src/components/statuses/InitialGreeting.jsx
@@ -4,15 +4,15 @@ const InitialGreeting = ({ hasError }) => {
   const statusMessage = hasError ? (
     <p>Oops, that was an error.</p>
   ) : (
-    <div>
-      <h3>
-        Find open source projects on GitHub by searching with labels, languages, or search terms!
-      </h3>
+    <div className="greeting-description">
+    <p>
+        Find open source projects on <a href="https://github.com/">GitHub</a> by searching with labels, languages, or search terms!
+      </p>
       <p>
         Use the toggles above to filter by common issue labels and by your favorite programming
         languages. You can also search by keyword!
       </p>
-      <p>Click Load Issues and get collaborating!</p>
+      <p><em>Click Load Issues and get collaborating!</em></p>
     </div>
   );
 
