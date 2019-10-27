53b56f36a23ff85382a7fda26c9175dcccfbd4ce
trybick <t.rybicki@gmail.com>
2019-10-27 11:22:06 -0400

    Replace header inline color with className

--- src/components/core/Header.jsx
+++ src/components/core/Header.jsx
@@ -5,7 +5,7 @@ import icon from '../../images/github-icon.png';
 const Header = () => (
   <div className="page-header">
     <h2 className="page-header-title">
-      <span style={{ color: '#3498db' }}>Issue</span>-Collab
+      <span className="page-header-title-highlight">Issue</span>-Collab
     </h2>
     <a
       className="github-link"
