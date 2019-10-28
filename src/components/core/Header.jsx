dc3fa7c1b29d0885762177e2d763c632ec8d90d6
trybick <t.rybicki@gmail.com>
2019-10-28 17:14:31 -0400

    Add github star badge and re-work header with bottom border

--- src/components/core/Header.jsx
+++ src/components/core/Header.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import './Header.scss';
 import icon from '../../images/github-icon.png';
+import GitHubButton from 'react-github-btn';
 
 const Header = () => (
   <div className="page-header">
     <h2 className="page-header-title">
       <span className="page-header-title-highlight">Issue</span>-Collab
     </h2>
-    <a
+    {/* <a
       className="github-link"
       href="https://github.com/trybick/issue-collab"
       target="_blank"
       rel="noopener noreferrer"
     >
       <img className="github-link-icon" src={icon} alt="github-logo" />
-    </a>
+    </a> */}
+
+    <GitHubButton
+      href="https://github.com/trybick/issue-collab"
+      data-icon="octicon-star"
+      data-show-count="true"
+      // data-size="large"
+      aria-label="Star trybick/issue-collab on GitHub"
+    >
+      Star
+    </GitHubButton>
   </div>
 );
 
