64be6663560823358e9f6ef39b04f7450bb67a26
trybick <t.rybicki@gmail.com>
2020-01-12 20:16:04 -0500

    Add google analytics

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-156066902-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
