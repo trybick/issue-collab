1756d059a0c0fd363b45cbcc22e27bd8ab461c96
Alex <tictak211@gmail.com>
2020-10-17 13:34:27 +0300

    keywords placeholder(styling-impr)

--- src/components/search/SearchContainer.jsx
+++ src/components/search/SearchContainer.jsx
@@ -21,7 +21,7 @@ const SearchContainer = ({
       <div className="search-container-input">
         <TextField
           margin="dense"
-          placeholder="Keywords (optional)"
+          placeholder="Enter keywords (optional)"
           className="search-container-input-field"
           type="text"
           variant="outlined"
