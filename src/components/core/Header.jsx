9b5cbaa30baca1fa5d24bfa34564291bc24de040
Owen Sessiecq <owen.sessiecq@ecomail.fr>
2020-10-17 12:39:32 +0200

    feat(dark-mode): Add dark mode to site (#48)

--- src/components/core/Header.jsx
+++ src/components/core/Header.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import './Header.scss';
 import githubIcon from '../../images/github-icon.svg';
+import githubIconWhite from '../../images/github-icon-white.svg';
+import Toggle from 'react-toggle';
 
-const Header = () => (
+const Header = ({ onToggleChangeDarkMode, darkMode }) => (
   <div className="page-header">
     <h2 className="page-header-title">
       <span className="page-header-title-highlight">Issue</span>-Collab
     </h2>
-
+    <div>
+      <Toggle
+          onChange={onToggleChangeDarkMode}
+      /> 
+      <span className="toggle-label">dark mode</span>
+    </div>
+    
     <a
       className="github-link"
       href="https://github.com/trybick/issue-collab"
       target="_blank"
       rel="noopener noreferrer"
     >
-      <img className="github-svg" src={githubIcon} alt="github-icon" />
+      <img className="github-svg" src={darkMode ? githubIconWhite : githubIcon} alt="github-icon"/>
     </a>
   </div>
 );
